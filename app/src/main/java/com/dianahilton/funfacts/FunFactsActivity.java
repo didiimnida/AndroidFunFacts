b84960738e51b83b844d37765e994261d54f01a4
Diana Hilton <diana.hilton@gmail.com>
2015-07-23 18:22:07 -0700

    Add files for Android App.

--- app/src/main/java/com/dianahilton/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/dianahilton/funfacts/FunFactsActivity.java
@@ -1,15 +1,22 @@
 package com.dianahilton.funfacts;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.Toast;
+import android.util.Log;
 
 
 public class FunFactsActivity extends Activity {
 
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
+
     private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +25,24 @@ public class FunFactsActivity extends Activity {
 
         //Declare our View variables and assign the Views from the layout file
         final TextView factLabel = (TextView) findViewById(R.id.complimentTextView);
-        Button showFactButton = (Button) findViewById(R.id.nextButton);
+        final Button showFactButton = (Button) findViewById(R.id.nextButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String compliment = mFactBook.getCompliment();
                 //Update the label with our dynamic compliment
                 factLabel.setText(compliment);
+                int color = mColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
 
             }
         };
         showFactButton.setOnClickListener(listener);
+
+        Toast.makeText(this, "Hi, Bee!", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "We're logging from the onCreate() method!");
     }
 }
